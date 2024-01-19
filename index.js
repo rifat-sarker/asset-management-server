b839ef8b75b74ef87ff5e16e71c22ac4da212f79
unknown <rifat-sarker>
2024-01-19 15:23:41 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,76 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const port = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+
+
+
+const uri = `mongodb+srv://${process.env.USER_DB}:${process.env.USER_PASS}@cluster0.l80xyen.mongodb.net/?retryWrites=true&w=majority`;
+
+
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    // await client.connect();
+
+    const customRequestCollection = client.db('assetDB').collection('customRequest')
+
+    // user custom request related apies
+    app.get('/custom', async(req,res)=>{
+      const result = await customRequestCollection.find().toArray();
+      res.send(result)
+    })
+
+
+    app.post('/custom', async(req,res)=>{
+      const customRequestItem = req.body;
+      const result = await customRequestCollection.insertOne(customRequestItem)
+      res.send(result)
+    })
+
+
+
+
+
+
+
+
+
+
+
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+app.get('/', (req,res)=>{
+    res.send('asset management Server running')
+})
+
+app.listen(port ,()=>{
+    console.log(`asset management server running on${port}`);
+})
