0e86a178897ffe751f9636832d2e1b9fd62f2c3f
unknown <rifat-sarker>
2024-01-22 00:01:07 +0600

    load custom request from db,get users from db

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const port = process.env.PORT || 5000;
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 
 // middleware
@@ -31,6 +31,7 @@ async function run() {
     // await client.connect();
 
     const customRequestCollection = client.db('assetDB').collection('customRequest')
+    const employeesCollection = client.db('assetDB').collection('employee')
 
     // user custom request related apies
     app.get('/custom', async(req,res)=>{
@@ -45,11 +46,27 @@ async function run() {
       res.send(result)
     })
 
+    app.get('/custom/:id', async(req,res)=>{
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)}
+      const result = await customRequestCollection.findOne(query)
+      res.send(result)
+    })
 
 
+  // employees related apies
+  app.get('/employees', async(req,res)=>{
+    const result = await employeesCollection.find().toArray();
+    res.send(result)
+  })
 
+  app.post('/employees', async(req,res)=>{
+    const user = req.body;
+    const result = await employeesCollection.insertOne(user)
+    res.send(result)
+  })
 
-
+  app.delete('/employeess')
 
 
 
